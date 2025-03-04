5f3401176af1e7b249736cc9acd8d7e900e88b8c
zgagalis <zgagalis@colostate.edu>
2025-03-03 20:16:29 -0700

    commit1

---
+++ Day-08.R
@@ -0,0 +1,9 @@
+# ESS 330 daily exercise 8
+# Feb. 27, 2025
+# Zoe Gagalis
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
+covid = read_csv(url)
+library(tidyverse)
+library(gapminder)
+library(ggplot2)
+library(dplyr)
