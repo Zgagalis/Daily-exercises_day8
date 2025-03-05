a452f1bfc20d228fb7c02085a32e4f4255adcb32
zgagalis <zgagalis@colostate.edu>
2025-03-05 10:13:28 -0700

    pls

--- Day-08.R
+++ Day-08.R
@@ -7,3 +7,19 @@ library(tidyverse)
 library(gapminder)
 library(ggplot2)
 library(dplyr)
+covid <- read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+
+dataframe <- data.frame(region = state.region,
+                 abbr = state.abb,
+                 state = state.name) 
+inner_join(dataframe, covid, by = "state") |>
+  group_by(region, date) |>
+  summarize(cases = sum(cases),
+            deaths = sum(deaths)) |>
+  pivot_longer(cols = c(cases, deaths),
+               names_to = "type",
+               values_to = "count") |>
+  ggplot(aes(x = date, y = count)) +
+  geom_line() +
+  facet_grid(type~region, scales = "free_y")+
+  theme_dark()
